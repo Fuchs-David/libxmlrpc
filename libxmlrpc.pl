cd328ca570f03bb6dd9eb1cbdc56661e41e486e6
David Fuchs <david.fuchs@seznam.cz>
2019-01-04 08:09:32 +0100

    Implemented predicate for registering external modules with the XML/RPC server. The library itself is now a module.

--- libxmlrpc.pl
+++ libxmlrpc.pl
@@ -1,3 +1,9 @@
+:- module(xmlrpc,[
+		server/0,	% Start server with default port of 8080.
+		server/1,	% Start server with an arbitrary port number.
+		%server/2,	% Start server with SSl context.
+		register/1	% Load module.
+	]).
 :- use_module(library('http/thread_httpd')).
 :- use_module(library('http/http_header')).
 :- use_module(library('http/http_dispatch')).
@@ -9,7 +15,9 @@
 
 :- http_handler('/', reply, []).
 
-% TODO: Register modules and predicates, which will be called before replying to client.
+% Register modules, which contain predicates to be called when requested by the client.
+% register(+Module)
+register(Module) :- use_module(Module).
 
 % Predicates to start a server
 server :- server(8080).
@@ -21,7 +29,7 @@ reply(Request) :-
 	member(method(post),Request),!,
 	% TODO: Read request data, parse XML, call registered predicate requested by remote procedure call
 	% and create response XML DOM.
-	
+
 	current_output(O),
 	set_stream(O,encoding(utf8)),
 	write('Content-type: application/xml'),
